5c5a4dd6df2496705253a9438ae7d49f9f8d853f
Rob Roos <1robroos@gmail.com>
2018-09-20 20:23:37 +0200

    small change

--- mytemplate.tf
+++ mytemplate.tf
@@ -1,14 +1,17 @@
 # Provider configuration
 provider "aws" {
-  region = "eu-central-1"
+  region                  = "eu-central-1"
   shared_credentials_file = "/home/rob/.aws/credentials"
   profile                 = "kfsoladmin"
 }
+
 # Resource configuration
 resource "aws_instance" "server1" {
-  ami = "ami-9bf712f4"
+  ami           = "ami-337be65c"
   instance_type = "t2.micro"
+
   tags {
     Name = "hello-instance"
+    Env = "test"
   }
 }
