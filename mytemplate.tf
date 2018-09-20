47bf7d9fb8ddaf56d967c0f4f96a7a1c1a8260a9
Rob Roos <1robroos@gmail.com>
2018-09-20 21:10:33 +0200

    added VPC and ignore changes

--- mytemplate.tf
+++ mytemplate.tf
@@ -5,13 +5,62 @@ provider "aws" {
   profile                 = "kfsoladmin"
 }
 
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  #Terraform, via interpolation syntax, allows us to reference any other resource it manages using the following syntax: ${RESOURCE_TYPE.RESOURCE_NAME.ATTRIBUTE_NAME}.
+  vpc_id     = "${aws_vpc.my_vpc.id}"
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP traffic"
+  vpc_id      = "${aws_vpc.my_vpc.id}"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 # Resource configuration
-resource "aws_instance" "server1" {
-  ami           = "ami-337be65c"
-  instance_type = "t2.micro"
+resource "aws_instance" "master-instance" {
+  ami                    = "ami-337be65c"
+  instance_type          = "t2.micro"
+  subnet_id              = "${aws_subnet.public.id}"
+  vpc_security_group_ids = ["${aws_security_group.allow_http.id}"]
 
   tags {
-    Name = "hello-instance"
-    Env = "test"
+    Name = "server-instance"
+    Env  = "test"
   }
+}
+
+resource "aws_instance" "server1" {
+  ami                         = "ami-337be65c"
+  instance_type               = "t2.micro"
+  subnet_id                   = "${aws_subnet.public.id}"
+  associate_public_ip_address = "True"
+
+  tags {
+    Name            = "client-instance"
+    Env             = "test"
+    master_hostname = "${aws_instance.master-instance.private_dns}"
+  }
+
+#  lifecycle {
+#    ignore_changes = ["tags"]
+#  }
 }
